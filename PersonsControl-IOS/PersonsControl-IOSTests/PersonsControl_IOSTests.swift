cd21c5e04ee2c308dc343719e11f88e1442eceaf
Valerii Melnikov <valeriimelnikov@MacBook-Pro-Valerij.local>
2018-08-14 03:57:54 +0300

    pods google

---
+++ PersonsControl-IOS/PersonsControl-IOSTests/PersonsControl_IOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  PersonsControl_IOSTests.swift
+//  PersonsControl-IOSTests
+//
+//  Created by Валерий Мельников on 14.08.2018.
+//  Copyright © 2018 Валерий Мельников. All rights reserved.
+//
+
+import XCTest
+@testable import PersonsControl_IOS
+
+class PersonsControl_IOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ PersonsControl-IOS/Pods/Target Support Files/Pods-PersonsControl-IOSUITests/Pods-PersonsControl-IOSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PersonsControl_IOSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PersonsControl_IOSUITests
+@end
