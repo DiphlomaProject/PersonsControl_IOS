86e388e363e733b75245ee6f3068ffb4cd16fb44
Valerii Melnikov <valeriimelnikov@MacBook-Pro-Valerij.local>
2018-08-14 15:38:21 +0300

    google sign in

--- PersonsControl-IOS/PersonsControl-IOS/ViewController.swift
+++ PersonsControl-IOS/PersonsControl-IOS/ViewController.swift
@@ -10,19 +10,44 @@ import UIKit
 import Firebase
 import GoogleSignIn
 class ViewController: UIViewController,GIDSignInUIDelegate {
-
+@IBOutlet weak var signInButton: GIDSignInButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupGoogleButtons()
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+         // setupGoogleButtons()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    fileprivate func setupGoogleButtons()
+/* fileprivate func setupGoogleButtons()
     { // создаем кнопку гугла
         let googleButton = GIDSignInButton()
         googleButton.frame = CGRect(x:60,y:500, width:view.frame.width - 115, height :50)
         view.addSubview(googleButton)
         GIDSignIn.sharedInstance()?.uiDelegate = self
+    }
+    */
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+        // ...
+        if let error = error {
+            // ...
+            return
+        }
+        
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                       accessToken: authentication.accessToken)
+        // ...
+        
+        Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
+            if let error = error {
+                // ...
+                return
+            }
+            // User is signed in
+            // ...
+        }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
