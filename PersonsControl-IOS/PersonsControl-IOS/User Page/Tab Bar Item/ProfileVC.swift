875c3b2fa2e3ba76d2487873db3ba9184f714737
Valerii Melnikov <valeriimelnikov@MacBook-Pro-Valerij.local>
2018-09-12 21:24:26 +0300

    update progile shared info

--- PersonsControl-IOS/PersonsControl-IOS/User Page/Tab Bar Item/ProfileVC.swift
+++ PersonsControl-IOS/PersonsControl-IOS/User Page/Tab Bar Item/ProfileVC.swift
@@ -13,10 +13,33 @@ import GoogleSignIn
 
 class ProfileVC: UIViewController {
 
+    @IBOutlet weak var displayName: UITextField!
+    
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var Role: UITextField!
+    
+    @IBOutlet weak var Phone: UITextField!
+    
+    @IBOutlet weak var address: UITextField!
+    
+    
+    @IBOutlet weak var city: UITextField!
+    
+    @IBOutlet weak var Country: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        
+            displayName.text = SingletonManager.sharedCenter.UserClass?.DisplayName
+            email.text = SingletonManager.sharedCenter.UserClass?.Email
+            Role.text = SingletonManager.sharedCenter.UserClass?.RoleName
+            Phone.text = SingletonManager.sharedCenter.UserClass?.PhoneNumber
+            address.text = SingletonManager.sharedCenter.UserClass?.Address
+            city.text = SingletonManager.sharedCenter.UserClass?.City
+            Country.text = SingletonManager.sharedCenter.UserClass?.Country
+        
     }
-    
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.isNavigationBarHidden = true
     }
